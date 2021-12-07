ee3ddfd61b2e8aa6682d3efaeaabf8c1d397265c
Gabriel Balbino <gbalbino098@gmail.com>
2021-12-06 23:00:46 -0300

    Entrar e Cadastrar

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,42 +1,16 @@
-import { environment } from './../../environments/environment.prod';
-import { Router } from '@angular/router';
-import { AuthService } from './../service/auth.service';
-import { UserLogin } from './../model/UserLogin';
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-entrar',
-  templateUrl: './entrar.component.html',
-  styleUrls: ['./entrar.component.css']
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.css']
 })
-export class EntrarComponent implements OnInit {
+export class InicioComponent implements OnInit {
 
-  userLogin: UserLogin = new UserLogin()
+  constructor() { }
 
-  constructor(
-    private auth: AuthService,
-    private router: Router
-  ) { }
-
-  ngOnInit() {
-    window.scroll(0,0)
-  }
-
-  entrar(){
-    this.auth.entrar(this.userLogin).subscribe((resp: UserLogin)=>{
-      this.userLogin = resp
-
-      environment.token = this.userLogin.token
-      environment.nome = this.userLogin.nome
-      environment.foto = this.userLogin.foto
-      environment.id = this.userLogin.id
-
-      this.router.navigate(['/inicio'])
-    }, erro =>{
-      if(erro.status == 500){
-        alert('Usuário ou senha estão incorretos!')
-      }
-    })
+  ngOnInit(): void {
   }
 
 }
