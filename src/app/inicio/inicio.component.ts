f77dbdd502f777d5a02b2e1ea7781cdf40867aed
Gabriel Balbino <gbalbino098@gmail.com>
2021-12-07 22:06:33 -0300

    update inicio

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,7 +1,6 @@
-
-import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from './../../environments/environment.prod';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-inicio',
@@ -12,16 +11,13 @@ export class InicioComponent implements OnInit {
 
   constructor(
     private router: Router
-  ) {}
+  ) { }
 
   ngOnInit() {
 
-    if(environment.token == '') {
-      alert('Sua seção expirou, faça o login novamente.')
+    if(environment.token == ''){
       this.router.navigate(['/entrar'])
     }
-    
-
   }
 
 }
