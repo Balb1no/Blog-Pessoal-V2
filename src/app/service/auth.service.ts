a477a3aa6185d46a4f6c93c0a6eaef4dd9ba31cb
Gabriel Balbino <gbalbino098@gmail.com>
2021-12-13 18:27:12 -0300

    post e get de postagem

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,9 @@
+import { environment } from './../../environments/environment.prod';
 import { UserLogin } from './../model/UserLogin';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../model/User';
-import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
@@ -21,6 +21,11 @@ export class AuthService {
   cadastrar(user: User): Observable<User> {
     return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
   }
+
+  getByIdUser(id: number): Observable<User> {
+    return this.http.get<User>(`http://localhost:8080/usuarios/${id}`)
+  }
+
 
   logado() {
     let ok: boolean = false
